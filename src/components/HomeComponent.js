a484562a5f4507c1243041d14aa0f7d7aa70133c
Srijay <parsi.srijay@gmail.com>
2020-05-19 09:19:28 +0530

    React Routers

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Home(props){
+	return(
+		<div classname="container">
+			<div classname="row">
+				<div classname="col-12">
+					<h4>Home</h4>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Home;
