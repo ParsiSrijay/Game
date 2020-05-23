1ede1f5e7f585d7d406a832edd61c25715f7e383
Srijay <parsi.srijay@gmail.com>
2020-05-23 09:28:14 +0530

    Fetch from server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle,Breadcrumb,Brea
 import { Link } from 'react-router-dom';
 import { Control,LocalForm,Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 	function RenderComments({comments,addComment,dishId})
 	{
@@ -33,7 +34,7 @@ import { Loading } from './LoadingComponent';
 	function RenderDishItem({dish}){
 		return(
 			<Card>
-				<CardImg width="100%" src={dish.image} alt={dish.name}/>
+				<CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
 				<CardBody>
 					<CardTitle>{dish.name}</CardTitle>
 					<CardText>{dish.description}</CardText>
